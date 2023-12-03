797f3dfaa18cdf9abf826fa625e81b995ec8cfba
vanilevsky <vanilevsky@gmail.com>
2023-12-03 10:06:50 +0700

    Add transcription

--- src/handlers/voices.ts
+++ src/handlers/voices.ts
@@ -1,61 +1,38 @@
 import Context from '@/models/Context'
 import bot from "@/helpers/bot";
 import { promises as fsPromises } from "fs";
-import OpenAI, { toFile } from "openai";
-import env from "@/helpers/env";
+import transcription from "@/helpers/openai";
 
 export default async function handleVoices(ctx: Context) {
-  const file = await ctx.getFile();
 
-  const ext = file.file_path?.split('.').pop();
-  const fileName = file.file_unique_id + '.' + ext;
-  const tmpFile = '/tmp/' + fileName;
-  const path = await file.download(tmpFile);
+  const chatId = ctx.message?.chat.id;
 
-  console.log("File saved at file.file_path: ", file.file_path);
-  console.log("File saved at path: ", path);
-
-  // Check if the file exists
-  fsPromises.access(tmpFile).then(() => {
-    console.log("File exists.");
-  }).catch(() => {
-    console.log("File does not exist.");
-  });
-
-  async function main() {
-    const buffer = await fsPromises.readFile(path);
-    const openai = new OpenAI({
-      apiKey: env.OPENAI_API_KEY,
-    });
-
-    const transcription = await openai.audio.transcriptions.create({
-      file: await toFile(buffer, fileName),
-      model: 'whisper-1',
-      language: ctx.from?.language_code,
-    });
-
-    console.log(transcription.text);
-
-    const { message } = ctx; // the message object
-    const chatId = message?.chat.id;
-
-    if (chatId) {
-      const text = '🔊 ' + transcription.text;
-      await bot.api.sendMessage(chatId, text, {
-        reply_to_message_id: message?.message_id,
-      });
-    }
+  if (chatId === undefined) {
+    console.error("chatId is undefined")
+    return
   }
 
-  await main();
+  const file = await ctx.getFile();
+  const fileExt = file.file_path?.split('.').pop();
+  const fileName = file.file_unique_id + '.' + fileExt;
+  const filePath = await file.download('/tmp/' + fileName);
 
-  // Remove tmp file tmpFile
-  await fsPromises.unlink(tmpFile);
-
-  // Check if the file exists
-  fsPromises.access(tmpFile).then(() => {
-    console.log("File exists.");
-  }).catch(() => {
-    console.log("File does not exist.");
+  fsPromises.access(filePath).catch(() => {
+    console.error("File does not exist.")
+    return
   });
+
+  const buffer = await fsPromises.readFile(filePath);
+  const transcriptionText = await transcription(buffer, fileName, ctx.from?.language_code);
+
+  const text = '🔊 ' + transcriptionText
+  await bot.api.sendMessage(chatId, text, {
+    reply_to_message_id: ctx.message?.message_id,
+  })
+
+  await fsPromises.unlink(filePath)
+
+  fsPromises.access(filePath).then(() => {
+    console.error("File exists.")
+  }).catch(() => {})
 }
---
+++ src/helpers/openai.ts
@@ -0,0 +1,17 @@
+import OpenAI, {toFile} from "openai";
+import env from "@/helpers/env";
+
+export default async function transcription(buffer: Buffer, fileName: string, lang: string | undefined): Promise<string> {
+
+    const openai = new OpenAI({
+        apiKey: env.OPENAI_API_KEY,
+    });
+
+    const transcription = await openai.audio.transcriptions.create({
+        file: await toFile(buffer, fileName),
+        model: 'whisper-1',
+        language: lang,
+    });
+
+    return transcription.text;
+}
