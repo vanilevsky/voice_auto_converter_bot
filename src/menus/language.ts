0c14583424f69fe4fad7dc20f417f2d8766942a5
vanilevsky <vanilevsky@gmail.com>
2023-12-04 09:07:47 +0700

    Debug

--- src/menus/language.ts
+++ src/menus/language.ts
@@ -4,6 +4,7 @@ import { load } from 'js-yaml'
 import { readFileSync, readdirSync } from 'fs'
 import { resolve } from 'path'
 import Context from '@/models/Context'
+import * as console from "console";
 
 interface YamlWithName {
   name: string
@@ -19,7 +20,14 @@ const localeFile = (path: string) => {
 
 const setLanguage = (languageCode: string) => async (ctx: Context) => {
   ctx.dbuser.language = languageCode
-  await ctx.dbuser.save()
+
+
+  console.log('await ctx.dbuser.save()', languageCode)
+
+  const res = await ctx.dbuser.save()
+
+  console.log(res)
+
   ctx.i18n.locale(languageCode)
   return ctx.editMessageText(ctx.i18n.t('language_selected'), {
     parse_mode: 'HTML',
@@ -30,6 +38,9 @@ const setLanguage = (languageCode: string) => async (ctx: Context) => {
 const languageMenu = new Menu<Context>('language')
 
 localeFilePaths.forEach((localeFilePath, index) => {
+
+  console.log(localeFilePath, index)
+
   const localeCode = localeFilePath.split('.')[0]
   const localeName = localeFile(localeFilePath).name
   languageMenu.text(localeName, setLanguage(localeCode))
