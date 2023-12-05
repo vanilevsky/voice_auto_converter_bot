426f64f328da5c2b2fc5acbbde3ff3ac6e2d7526
vanilevsky <vanilevsky@gmail.com>
2023-12-05 08:06:48 +0700

    Add webhook endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,29 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express'
 
-const app = express();
-const port = 80;
+const app = express()
+const port = 80
 
-app.use(express.json());
+app.use(express.json())
 
 app.use((req: Request, res: Response, next: express.NextFunction) => {
-  console.log(`Received a ${req.method} request on ${req.path}`);
-  console.log('Headers:', req.headers);
-  console.log('Body:', req.body);
-  next();
-});
+  console.log(`Received a ${req.method} request on ${req.path}`)
+  console.log('Headers:', req.headers)
+  console.log('Body:', req.body)
+  next()
+})
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('ok');
-});
+  res.send('Hello World!')
+})
+
+app.get('/webhook', (req: Request, res: Response) => {
+  res.send('ok')
+})
+
+app.post('/webhook', (req: Request, res: Response) => {
+  res.send('ok')
+})
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+  console.log(`Server is running at http://localhost:${port}`)
+})
