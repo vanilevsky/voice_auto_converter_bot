25020625d08eec11f0c70dc0c33ee818f9246a1f
vanilevsky <vanilevsky@gmail.com>
2023-12-17 13:21:22 +0700

    Update translations

--- src/handlers/statistic.ts
+++ src/handlers/statistic.ts
@@ -2,16 +2,15 @@ import Context from '@/models/Context'
 import sendOptions from '@/helpers/sendOptions'
 
 export default function handleStatistic(ctx: Context) {
-  // I have transcribed 6 voice messages for you common length of 10 seconds.
-  // I have transcribed 30 circle video messages for you common length of 10 seconds.
   const userStatistic = ctx.dbuser.statistic
-
   const text = ctx.i18n.t('statistic', {
-    sinceTime: 'yesterday',
-    voiceTotalCount: 5,
-    videoNoteTotalCount: 15,
-    voiceTotalLength: '10 seconds',
-    videoNoteTotalLength: '25 seconds',
+    sinceTime: userStatistic.timeStamps?.createdAt?.toLocaleDateString(
+      ctx.dbuser.language
+    ),
+    voiceTotalCount: userStatistic.voice.count_total,
+    videoNoteTotalCount: userStatistic.video_note.count_total,
+    voiceTotalLength: userStatistic.voice.duration,
+    videoNoteTotalLength: userStatistic.video_note.duration,
   })
 
   return ctx.reply(text, {
