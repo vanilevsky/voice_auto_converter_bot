df91d649d20fc55a6b57138dff5228d0f215c246
vanilevsky <vanilevsky@gmail.com>
2024-08-24 10:14:58 +0200

    Fix language file

--- src/menus/language.ts
+++ src/menus/language.ts
@@ -4,7 +4,6 @@ import { load } from 'js-yaml'
 import { readFileSync, readdirSync } from 'fs'
 import { resolve } from 'path'
 import Context from '@/models/Context'
-import * as console from "console";
 
 interface YamlWithName {
   name: string
@@ -21,7 +20,7 @@ const localeFile = (path: string) => {
 const setLanguage = (languageCode: string) => async (ctx: Context) => {
   ctx.dbuser.language = languageCode
 
-  const res = await ctx.dbuser.save()
+  await ctx.dbuser.save()
 
   ctx.i18n.locale(languageCode)
   return ctx.editMessageText(ctx.i18n.t('language_selected'), {
@@ -33,7 +32,6 @@ const setLanguage = (languageCode: string) => async (ctx: Context) => {
 const languageMenu = new Menu<Context>('language')
 
 localeFilePaths.forEach((localeFilePath, index) => {
-
   const localeCode = localeFilePath.split('.')[0]
   const localeName = localeFile(localeFilePath).name
   languageMenu.text(localeName, setLanguage(localeCode))
